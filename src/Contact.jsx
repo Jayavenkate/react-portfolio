eed70054e9d2503638dfbdaae7262af99231ff5c
Jayavenkate <jayajaikithuja@gmail.com>
2023-05-20 18:51:43 +0300

    contact

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,22 +1,36 @@
-import { Card } from "@mui/material";
+import { Card, IconButton } from "@mui/material";
 import { Element } from "react-scroll";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import "./Contact.css";
 export function Contact() {
   return (
     <Element id="contact" className="contact-container">
       <div className="contact-con">
         <div className="contact-content">
-        
           <h2>Contact</h2>
         </div>
         <Card className="contact-card">
-         
           <h3>
             Email : <span>jayajaikithuja@gmail.com</span>
           </h3>
           <h3>
             Github UserName: <span>Jayavenkate</span>
           </h3>
+          <div>
+            <IconButton
+              onClick={() => window.open("https://github.com/Jayavenkate")}
+            >
+              <GitHubIcon />
+            </IconButton>
+            <IconButton
+              onClick={() =>
+                window.open("https://www.linkedin.com/in/jaya-j-281343269/")
+              }
+            >
+              <LinkedInIcon />
+            </IconButton>
+          </div>
         </Card>
       </div>
     </Element>
--- src/Home.jsx
+++ src/Home.jsx
@@ -6,16 +6,21 @@ export function Home() {
     <Element name="home" className="top-container">
       <div className="home-content">
         <div className="home-container">
-          <h1>Hi ,I am </h1>
-          <h1>Jaya </h1>
-          <h1>Full stack developer</h1>
+          <h1 className="span-1">
+            <span>Hi, I am </span>
+          </h1>
+          <h1 className="span-2">
+            <span>Jaya</span>{" "}
+          </h1>
+          <h1 className="span-3">
+            <span>Full stack developer</span>
+          </h1>
           {/* <div className="home-button"> */}
-          <a href="www.google.com">
-            <Button variant="contained" color="error" className="btn">
-              {" "}
+          {/* <a href="https://drive.google.com/file/d/1ZO6RjSeaAAta9Y_FTMOq2tLc6kxIAtRr/view?usp=share_link"> */}
+            <Button onClick={()=>window.open("https://drive.google.com/file/d/1ZO6RjSeaAAta9Y_FTMOq2tLc6kxIAtRr/view?usp=share_link")}variant="contained" color="error" className="btn">
               Resume
             </Button>
-          </a>
+          {/* </a> */}
           {/* <Link to="project" smooth={true} duration={500}>
             <Button variant="contained"> My Work </Button>
           </Link> */}
